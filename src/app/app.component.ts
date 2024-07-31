aa0a2fd3e071da14f450a596394f4d936cd4143b
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2024-07-30 20:12:23 -0400

    Initial commit with api, and user registraion logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+  // This will open the dialog when the signup button is clicked
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      //assign the dialog a width
+      width: '300px'
+    });
+  }
+
+
 }
---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,110 @@
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError, map } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from
+  '@angular/common/http';
+
+// Declaring the api url that will funnel data for the client app
+const apiUrl = 'https://movie-selector.onrender.com/';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FetchApiDataService {
+  constructor(private http: HttpClient) { }
+
+  // User registration
+  public userRegistration(userDetails: any): Observable<any> {
+    return this.http.post(apiUrl + 'users', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // User  login
+  public userLogin(userDetails: any): Observable<any> {
+    return this.http.post(apiUrl + 'login', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get all movies
+  public getAllMovies(): Observable<any> {
+    return this.http.get(apiUrl + 'movies').pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get one movie
+  public getMovie(title: string): Observable<any> {
+    return this.http.get(apiUrl + `movies/${title}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get Director
+  public getDirector(directorName: string): Observable<any> {
+    return this.http.get(apiUrl + `directors/${directorName}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get genre
+  public getGenre(genreName: string): Observable<any> {
+    return this.http.get(apiUrl + `genres/${genreName}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Get user
+  public getUser(userName: string): Observable<any> {
+    return this.http.get(apiUrl + `users/${userName}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+  // Get Favorite Movies
+  public getFavoriteMovies(userName: string): Observable<any> {
+    return this.http.get(apiUrl + `users/${userName}/movies`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Add to Favorite Movies
+  public getFavoriteMovie(userName: string, movieID: string): Observable<any> {
+    return this.http.post(apiUrl + `users/${userName}/movies/${movieID}`, {}).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Edit User
+  public editUser(userName: string, userDetails: any): Observable<any> {
+    return this.http.put(apiUrl + `users/${userName}`, userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Delete User
+  public deleteUser(userName: string): Observable<any> {
+    return this.http.delete(apiUrl + `users/${userName}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  // Remove a movie from favorite movies
+  public deleteFavoriteMovie(userName: string, movieID: string): Observable<any> {
+    return this.http.delete(apiUrl + `users/${userName}/movies/${movieID}`).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status},` +
+        `Error body is: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+}
