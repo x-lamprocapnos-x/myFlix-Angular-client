2b16b7462c854d84b5ea5fb4958bf38115ce59a1
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2024-08-01 20:17:23 -0400

    add console.logs for userRegistration

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -25,10 +25,12 @@ export class UserRegistrationFormComponent implements OnInit {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
       // Logic for a successful user registation goes here ( coming soon )
       this.dialogRef.close(); // This will close the modal on success
+      console.log(result);
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
     }, (result) => {
+      console.log(result);
       this.snackBar.open(result, 'OK', {
         duration: 2000
       });
