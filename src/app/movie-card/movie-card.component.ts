453b95ed97c0717cc6f0d3bb8dbece81738066e0
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2024-08-04 14:15:31 -0400

    create movie-card comp. add logic, and rendering

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrl: './movie-card.component.css'
+})
+export class MovieCardComponent {
+  movies: any[] = [];
+  constructor(public fetchApiData: FetchApiDataService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((result: any) => {
+      this.movies = result;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+
+}
