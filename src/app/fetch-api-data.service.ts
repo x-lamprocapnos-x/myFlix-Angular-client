7d67b9d1d62711b474f362f62631f10dbf4919c0
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2024-08-13 19:36:38 -0400

    add Authorization to endpoints

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -13,6 +13,12 @@ const apiUrl = 'https://movie-selector.onrender.com/';
 export class FetchApiDataService {
   constructor(private http: HttpClient) { }
 
+  // get token
+  private getToken(): string {
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user).token : '';
+  }
+
   // User registration
   public userRegistration(userDetails: any): Observable<any> {
     return this.http.post(apiUrl + 'users', userDetails).pipe(
@@ -27,71 +33,113 @@ export class FetchApiDataService {
     );
   }
 
-  // Get all movies
+  // Get all movies with token attached for access
   public getAllMovies(): Observable<any> {
-    return this.http.get(apiUrl + 'movies').pipe(
-      catchError(this.handleError)
-    );
+    const token = localStorage.getItem('token');
+
+    return this.http.get(apiUrl + 'movies', {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      }),
+    }).pipe(
+      catchError(this.handleError));
   }
 
-  // Get one movie
+  // Get one movie with token attached for access
   public getMovie(title: string): Observable<any> {
-    return this.http.get(apiUrl + `movies/${title}`).pipe(
+    return this.http.get(apiUrl + `movies/${title}`, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }).pipe(
       catchError(this.handleError)
     );
   }
 
-  // Get Director
+  // Get Director with token attached for access
   public getDirector(directorName: string): Observable<any> {
-    return this.http.get(apiUrl + `directors/${directorName}`).pipe(
+    return this.http.get(apiUrl + `directors/${directorName}`, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }).pipe(
       catchError(this.handleError)
     );
   }
 
-  // Get genre
+  // Get genre with token attached for access
   public getGenre(genreName: string): Observable<any> {
-    return this.http.get(apiUrl + `genres/${genreName}`).pipe(
+    return this.http.get(apiUrl + `genres/${genreName}`, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }).pipe(
       catchError(this.handleError)
     );
   }
 
-  // Get user
+  // Get user with token attached for access
   public getUser(userName: string): Observable<any> {
-    return this.http.get(apiUrl + `users/${userName}`).pipe(
+    return this.http.get(apiUrl + `users/${userName}`, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }).pipe(
       catchError(this.handleError)
     );
   }
-  // Get Favorite Movies
+  // Get Favorite Movies with token attached for access
   public getFavoriteMovies(userName: string): Observable<any> {
-    return this.http.get(apiUrl + `users/${userName}/movies`).pipe(
+    return this.http.get(apiUrl + `users/${userName}/movies`, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }).pipe(
       catchError(this.handleError)
     );
   }
 
-  // Add to Favorite Movies
+  // Add to Favorite Movies with token attached for access
   public addFavoriteMovie(userName: string, movieID: string): Observable<any> {
-    return this.http.post(apiUrl + `users/${userName}/movies/${movieID}`, {}).pipe(
+    return this.http.post(apiUrl + `users/${userName}/movies/${movieID}`, {}, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }).pipe(
       catchError(this.handleError)
     );
   }
 
-  // Edit User
+  // Edit User with token attached for access
   public editUser(userName: string, userDetails: any): Observable<any> {
-    return this.http.put(apiUrl + `users/${userName}`, userDetails).pipe(
+    return this.http.put(apiUrl + `users/${userName}`, userDetails, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }).pipe(
       catchError(this.handleError)
     );
   }
 
-  // Delete User
+  // Delete User with token attached for access
   public deleteUser(userName: string): Observable<any> {
-    return this.http.delete(apiUrl + `users/${userName}`).pipe(
+    return this.http.delete(apiUrl + `users/${userName}`, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }
+    ).pipe(
       catchError(this.handleError)
     );
   }
 
-  // Remove a movie from favorite movies
+  // Remove a movie from favorite movies with token attached for access
   public deleteFavoriteMovie(userName: string, movieID: string): Observable<any> {
-    return this.http.delete(apiUrl + `users/${userName}/movies/${movieID}`).pipe(
+    return this.http.delete(apiUrl + `users/${userName}/movies/${movieID}`, {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.getToken()}`
+      })
+    }).pipe(
       catchError(this.handleError)
     );
   }
