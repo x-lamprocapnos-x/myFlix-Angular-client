1e024fade8ebf5f0079075e544cd4d26b01242f1
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2024-10-08 20:12:14 -0400

    add block comments to align with typedoc syntax

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,3 +1,7 @@
+/**
+ * This component is responsible for displaying movie cards
+ * and handling interactions like adding/removing favorites.
+ */
 import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { Router } from '@angular/router';
@@ -9,22 +13,30 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrl: './movie-card.component.css'
 })
 export class MovieCardComponent implements OnInit {
+  /** Array to hold the movie data. */
   movies: any[] = [];
 
+  /** Template reference for the dialog to display movie details. */
   @ViewChild('dialogTemplate') dialogTemplate!: TemplateRef<any>;
 
+  /**
+   * Constructor for the MovieCardComponent.
+   * @param fetchApiData - The service to fetch API data.
+   * @param router - The Angular Router to handle navigation.
+   * @param dialog - The Angular Material Dialog service to open dialogs.
+   */
   constructor(
     public fetchApiData: FetchApiDataService,
     public router: Router,
     public dialog: MatDialog
-  ) {
+  ) { }
 
-  }
-
+  /** Lifecycle hook that runs when the component is initialized. */
   ngOnInit(): void {
     this.getMovies();
   }
 
+  /** Fetches all movies from the API and checks if they are marked as favorites.*/
   getMovies(): void {
     this.fetchApiData.getAllMovies().subscribe((result: any) => {
       this.movies = result;
@@ -37,7 +49,7 @@ export class MovieCardComponent implements OnInit {
       }
 
       this.movies.forEach((movie: any) => {
-        movie.isFavorite = user.FavoriteMovies.includes(movie._id) /* check movie._id? or ._Id?*/
+        movie.isFavorite = user.FavoriteMovies.includes(movie._id)
       })
       return this.movies;
     }, error => {
@@ -45,11 +57,17 @@ export class MovieCardComponent implements OnInit {
     })
   }
 
+  /** Logs out user, clears local storage, and navigates to the welcome page. */
   logout(): void {
     this.router.navigate(['welcome']);
     localStorage.removeItem('user');
   }
 
+  /**
+   * Opens dialog with additional movie details based on the type (genre, director, or description).
+   * @param movie - The movie to display details for.
+   * @param type - The type of information to display ('genre', 'director', or 'description').
+   */
   openMoviesDialog(movie: any, type: string): void {
     console.log('Genre', movie.Genre.Name);
     const data = {
@@ -59,18 +77,21 @@ export class MovieCardComponent implements OnInit {
         : type === 'director' ? movie.Director.map((director: any) => director.Name).join(', ')
           : movie.Description
     };
-    console.log('Opening dialog with data:', data);
     this.dialog.open(this.dialogTemplate, {
       width: '600px',
       data: data
     });
   }
 
+  /** Redirects the user to their profile page. */
   redirectProfile(): void {
     this.router.navigate(['profile']);
   }
 
-
+  /**
+   * Toggles the favorite status of a movie for the logged-in user.
+   * @param movie - The movie to toggle as favorite.
+   */
   toggleFavorite(movie: any): void {
     const user = JSON.parse(localStorage.getItem('user') || '');
     const isFavorite = user.FavoriteMovies.includes(movie._id);
